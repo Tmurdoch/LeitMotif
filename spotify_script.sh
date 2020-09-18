9386d94db2b22ff4faf9c951431ce8c38635ac46
Thomas Murdoch <thomasjmurd@gmail.com>
2020-09-17 20:44:49 -0600

    updated plan

--- spotify_script.sh
+++ spotify_script.sh
@@ -1,8 +1,14 @@
 
 #! /bin/bash
 
-#purpose: call spotify for request and get current song name + title, etc.,
+#purpose: call spotify for request (and access to user data) 
+#and get current song name + title, etc.,
 #output should be pipelinable
+
+
+#use implicit grant flow method
+#https://stackoverflow.com/questions/31092808/is-it-possible-to-use-the-spotify-web-api-to-write-a-desktop-application-without
+
 
 
 echo "yo"
