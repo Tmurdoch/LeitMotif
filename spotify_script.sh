9eb9669f2f647dbcbe51daa4edf485c224a7ea7d
Thomas Murdoch <thomasjmurd@gmail.com>
2020-09-19 12:18:25 -0600

    GET updated

--- spotify_script.sh
+++ spotify_script.sh
@@ -1,6 +1,6 @@
 
 #! /bin/bash
-
+set -x
 #purpose: call spotify for request (and access to user data) 
 #and get current song name + title, etc.,
 #output should be pipelinable
@@ -9,9 +9,10 @@
 #use implicit grant flow method
 #https://stackoverflow.com/questions/31092808/is-it-possible-to-use-the-spotify-web-api-to-write-a-desktop-application-without
 
-GET https://accounts.spotify.com/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09
+#GET https://accounts.spotify.com/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09
 
-#TODO: scope should be user-read-playback-state, also change URI to whitelisted one in dashboard
+GET https://accounts.spotify.com/authorize?client_id=a211de10b38d44b1a4874a0d4281a8aa&response_type=code&redirect_uri=my-awesome-app-login://callback&scope=user-read-playback-state
 
-echo "yo"
+
+#TODO: cat this to an html file, then open it in a browser, sign in wait for response
 
