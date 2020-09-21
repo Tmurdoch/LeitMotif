3e733dd270cd778b71c225f9399f33cee13b92e6
Thomas Murdoch <tjm149@usask.ca>
2020-09-20 21:53:56 -0600

    trying to just open html in google

--- spotify_script.sh
+++ spotify_script.sh
@@ -11,7 +11,10 @@ set -x
 
 #GET https://accounts.spotify.com/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09
 
-GET https://accounts.spotify.com/authorize?client_id=a211de10b38d44b1a4874a0d4281a8aa&response_type=code&redirect_uri=my-awesome-app-login://callback&scope=user-read-playback-state
+GET https://accounts.spotify.com/authorize?client_id=a211de10b38d44b1a4874a0d4281a8aa&response_type=code&redirect_uri=my-awesome-app-login://callback&scope=user-read-playback-state > web.html
+
+#TODO: still printing to stdin, ^^^^^ not redirected correctly
+google-chrome --no-sandbox --disable-setuid-sandbox web.html
 
 
 #TODO: cat this to an html file, then open it in a browser, sign in wait for response
