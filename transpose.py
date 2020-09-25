9ef9b23bff0de8bede818024f419e661a5e5ec39
programmer-a <ProgA@cs.usask.ca>
2020-09-24 18:56:39 -0600

    added stub file transpose.py

---
+++ transpose.py
@@ -0,0 +1,11 @@
+# purpose of this program is to transpose a melody to different key
+
+#!/usr/bin/python
+import sys
+
+#get key and BPM from command line
+
+key= sys.argv[1]
+BPM = sys.argv[2]
+
+print(key, BPM)
